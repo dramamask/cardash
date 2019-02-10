1aafd98faa9eb544009e2c7fa5bcb040416cc182
Mark <mjtdaams@gmail.com>
2019-02-09 20:49:30 -0700

    Add drawing of a line

--- cpp/canvas.h
+++ cpp/canvas.h
@@ -6,4 +6,8 @@ class Canvas : public Gtk::DrawingArea
     public:
         Canvas();
         virtual ~Canvas();
+        
+    protected:
+        // Override default signal handler:
+        bool on_draw(const Cairo::RefPtr<Cairo::Context>& cr) override;    
 };
